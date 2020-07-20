42d17d03d446c906041957da5371cc9481b64007
shivamgupta <shivam@soroco.com>
2020-07-20 23:47:59 +0530

    delete imgs

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function setColorHoverListener() {
 /* Photos */
 
 function setRandomPhoto() {
-  let num = Math.floor(Math.random() * 4) + 1;
+  let num = Math.floor(Math.random() * 2) + 1;
   document.getElementById("propic").src = `./img/face${num}.jpeg`;
 }
 
