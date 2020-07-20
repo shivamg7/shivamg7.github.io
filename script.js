69e68049b2c3c660702e06a3f32d4ee101c36864
shivamgupta <shivam@soroco.com>
2020-07-20 23:34:29 +0530

    add faces

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function setColorHoverListener() {
 /* Photos */
 
 function setRandomPhoto() {
-  let num = Math.floor(Math.random() * 14) + 1;
+  let num = Math.floor(Math.random() * 4) + 1;
   document.getElementById("propic").src = `./img/face${num}.jpg`;
 }
 
